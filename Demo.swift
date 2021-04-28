a4f66b38fe9bb7fe094954b10e059bc066b5d685
kene <yonghu@lightdeiMac.local>
2021-04-28 09:54:17 +0800

    Initial KenePodTest

---
+++ Demo.swift
@@ -0,0 +1,14 @@
+//
+//  Demo.swift
+//  KenePodTest
+//
+//  Created by kene on 2021/4/28.
+//
+
+import UIKit
+
+class Demo: NSObject {
+    func test() {
+        print("this is a demo...")
+    }
+}
---
+++ Example/Pods/Target Support Files/KenePodTest/KenePodTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_KenePodTest : NSObject
+@end
+@implementation PodsDummy_KenePodTest
+@end
